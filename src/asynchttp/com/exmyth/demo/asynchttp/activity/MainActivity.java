e7cbcb8cf22379b14e2baa007f7c47fefd5e9b2a
exmyth <iexmyth@gmail.com>
2016-01-27 15:36:49 +0800

    init demo

---
+++ src/asynchttp/com/exmyth/demo/asynchttp/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.exmyth.demo.asynchttp.activity;
+
+import com.exmyth.demo.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
